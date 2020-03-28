b9fcb8a3a8c43dc613fad0ba19c23bf914041299
Adrián Quiles Arias <adrianquilesarias@iMac-de-Adrian.local>
2020-03-28 18:42:23 +0100

    Add Responsive Moods

--- js/hide.js
+++ js/hide.js
@@ -108,3 +108,14 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
